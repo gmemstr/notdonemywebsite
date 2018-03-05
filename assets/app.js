392016837b43b7fa323418f31625f9d5e14a4e9f
Gabriel Simmer <bladesimmer@gmail.com>
2018-03-04 17:06:06 -0800

    Getting different template on button press works

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,16 @@
 console.log("app.js loaded");
 
 var getTemplateHtml = template => {
-	fetch('template/template.html')
+	document.getElementById('iframe').src = "about:blank";
+	fetch('template/'+template+'.html')
 	.then(function(response) {
 		return response.text()
 	})
 	.then(function(html) {
 		document.getElementById('iframe').contentWindow.document.write(html);
+		console.log(document.getElementById('iframe').contentWindow.document)
 	});
 }
-
-getTemplateHtml("1");
 
 var valueChange = value => {
 	var iframehtml = document.getElementById('iframe').contentWindow.document.getElementById(value);
