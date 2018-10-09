18a4b4f42853c0a4342b840db21e9fd2b4fb0765
Ovidiu Alexandru Toma <inoxovi@gmail.com>
2018-10-09 15:31:11 +0300

    hotfix/add_color - wip

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,14 @@ var getTemplateHtml = template => {
 	})
 	.then(function(html) {
 		document.getElementById('iframe').contentWindow.document.write(html);
-		console.log(document.getElementById('iframe').contentWindow.document)
+		// console.log(document.getElementById('iframe').contentWindow.document)
+
+		var elements = document.getElementById('iframe').contentWindow.document.querySelectorAll("body *");
+		var numElements = elements.length;
+		var i;
+		for (i = 0; i < numElements; i++) {
+			elements[i].addEventListener("click", addColorTrigger);
+		}
 	});
 }
 
@@ -16,6 +23,12 @@ var valueChange = value => {
 	var iframehtml = document.getElementById('iframe').contentWindow.document.getElementById(value);
 	val = document.getElementById(value).value;
 	iframehtml.innerHTML = val
+}
+
+var addColorTrigger = (e) => {
+	console.log("click");
+	console.log(e.currentTarget);
+
 }
 
 var downloadPage = () => {
