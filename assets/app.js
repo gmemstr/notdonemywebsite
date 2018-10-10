c7b784b1266ff3beb16c4ea938ecbed96a5f6546
Ovidiu Alexandru Toma <inoxovi@gmail.com>
2018-10-10 18:13:38 +0300

    adds implementation

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 console.log("app.js loaded");
 
-var getTemplateHtml = template => {
+var getTemplateHtml = (template, primary_color="#ffffff", secondary_color="#ffffff") => {
 	document.getElementById('iframe').src = "about:blank";
 	fetch('template/'+template+'.html')
 	.then(function(response) {
@@ -9,26 +9,40 @@ var getTemplateHtml = template => {
 	.then(function(html) {
 		document.getElementById('iframe').contentWindow.document.write(html);
 		// console.log(document.getElementById('iframe').contentWindow.document)
+		primary_elements = document.getElementById('iframe').contentWindow.document.getElementsByClassName("primary_color");
+		applyColor(primary_elements, primary_color);
 
-		var elements = document.getElementById('iframe').contentWindow.document.querySelectorAll("body *");
-		var numElements = elements.length;
-		var i;
-		for (i = 0; i < numElements; i++) {
-			elements[i].addEventListener("click", addColorTrigger);
-		}
+		secondary_elements = document.getElementById('iframe').contentWindow.document.getElementsByClassName("secondary_color");
+		applyColor(secondary_elements, secondary_color);
+		document.getElementById("primary").value = primary_color
+		document.getElementById("secondary").value = secondary_color
 	});
 }
 
+var changeColor = (el) => {
+	color = el.value
+	iframe_doc = document.getElementById('iframe').contentWindow.document
+	if(el.id == "primary")
+	{
+		divs = iframe_doc.getElementsByClassName("primary_color")
+	}
+	else
+	{
+		divs = iframe_doc.getElementsByClassName("secondary_color")
+	}
+	applyColor(divs, color);
+}
+
+var applyColor = (elements, color) => {
+	var i;
+	for (i = 0; i < elements.length; i++) { 
+	  elements[i].style["background-color"] = color;
+	}
+}
 var valueChange = value => {
 	var iframehtml = document.getElementById('iframe').contentWindow.document.getElementById(value);
 	val = document.getElementById(value).value;
 	iframehtml.innerHTML = val
-}
-
-var addColorTrigger = (e) => {
-	console.log("click");
-	console.log(e.currentTarget);
-
 }
 
 var downloadPage = () => {
